c9caca440b890dfca968b30b762a0c8cbf455a99
Jacopo <jmangia@me.com>
2017-09-07 16:59:32 -0700

    Adding iOS Extended project with Male/Female parameters

---
+++ iOS Extended Project/HeightWeightCoreMLKerasTest/ViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ViewController.swift
+//  HeightWeightCoreMLKerasTest
+//
+//  Created by Jacopo Mangiavacchi on 9/1/17.
+//  Copyright © 2017 JacopoMangia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var inputText: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var unitControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func calculateHeight(_ sender: Any) {
+        if let input = Float(inputText.text!) {
+            let modelWrapper = HeightWeightModelWrapper()
+
+            var resultString = ""
+            if unitControl.selectedSegmentIndex == 1 {
+                let result = modelWrapper.predictHeight(cm: input)
+                resultString = "\(result) Kilos"
+            }
+            else {
+                let result = modelWrapper.predictHeight(inches: input)
+                resultString = "\(result) Pounds"
+            }
+
+            resultLabel.text = "Your expected Weight should be \(resultString)"
+            resultLabel.isHidden = false
+        }
+    }
+    
+}
+
